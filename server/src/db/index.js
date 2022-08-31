0460168359c8ad663721c6123d74314e1ed825e0
Emi2710 <deadourfreedom@gmail.com>
2022-08-31 17:57:38 +0200

    auth (backend)

---
+++ server/src/db/index.js
@@ -0,0 +1,15 @@
+const { config } = require('dotenv')
+config()
+
+const { Pool } = require('pg')
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'pern_auth',
+  password: process.env.PG_PASSWORD,
+  port: 5432,
+})
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+}
