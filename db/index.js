c790353bb5da491e2075ff940c91cbddd4f38ef8
Emi2710 <deadourfreedom@gmail.com>
2022-10-11 21:57:24 +0200

    updated db connection parameters

--- db/index.js
+++ db/index.js
@@ -3,21 +3,24 @@ config()
 
 const { Pool } = require('pg')
 
-const devConfig = {
+/*const devConfig = {
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
-}
+}*/
 
-const proConfig = {
-  connectionString: process.env.DATABASE_URL
-}
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const pool = new Pool(
-  process.env.NODE_ENV === 'production' ? proConfig : devConfig
-)
+
+const proConfig = process.env.DATABASE_URL
+
+
+const pool = new Pool({
+  connectionString:
+    process.env.NODE_ENV === "production" ? proConfig : devConfig,
+})
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
