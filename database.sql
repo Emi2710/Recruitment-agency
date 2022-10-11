d7062a672d040069b002f65b5140c5f256ac7281
Emi2710 <deadourfreedom@gmail.com>
2022-10-11 16:14:32 +0200

    database.sql

--- database.sql
+++ database.sql
@@ -1,11 +1,7 @@
 -- users table
-create table users(
-    user_id serial primary key,
-    email varchar(255) unique not null,
-    password varchar(255) not null,
-    role varchar(255)
-    created_at date default current_date
-);
+
+create table users( user_id serial primary key, email varchar(255) unique not null, password varchar(255) not null, role varchar(255) created_at date default current_date);
 
 
+create table crud_recruteur(job_id serial primary key, job_title varchar, job_place varchar, job_description varchar, author varchar);
 
